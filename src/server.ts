d9566c43f0da3f43434d672fc8e240d6992dfc70
delta <528491526@qq.com>
2025-05-27 10:23:27 +0800

    初步可以运行

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,29 @@
+import Router from '@koa/router';
+import fs from 'fs';
+import { readFile } from 'fs/promises';
 import Koa from 'koa';
+import path from 'path';
 import koaServe from './koa-serve.middleware';
 
-
-export const boot = (prot:number,directories: string[] | string,root:string) => {
+export const boot = (prot: number,directories: string[] | string,root: string) => {
     const app = new Koa();
     app.use(koaServe(directories,root));
+    const router = new Router();
+    router.get(/.*/,async (ctx) => {
+        const indexPath = path.join(root,'index.html');
+        const reqPath = ctx.request.path;
+        const assetsPath = path.join(root,reqPath);
+        if (fs.existsSync(assetsPath)) {
+            const stat = fs.lstatSync(assetsPath);
+            if (!stat.isDirectory()) {
+                ctx.type = path.extname(assetsPath);
+                ctx.body = fs.createReadStream(assetsPath);
+                return;
+            }
+        }
+        const file = await readFile(indexPath,'utf-8');
+        ctx.body = file;
+    });
+    app.use(router.routes());
     app.listen(prot);
 };   
